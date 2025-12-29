1edade1753b93f9b7a851c2e81da3585c4b799ee
benmaa <benmaa942@gmail.com>
2025-12-29 18:35:24 +0100

    save servesj

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,7 +18,10 @@ const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(rateLimiter);
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(cookieParser());
 
 app.use("/api/todos", todoRoutes);
